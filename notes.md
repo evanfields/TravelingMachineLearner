0b0361f158d2f74326754c43756437084144c0d1
Evan Fields <evanjeromefields@gmail.com>
2024-05-02 12:27:38 -0400

    exploring architectures

--- notes.md
+++ notes.md
@@ -14,3 +14,30 @@ to predict that when we haven't visited any cities yet we start at city 1?
 training data?
 * For a given TSP instance, should we sort the cities to provide a more predictable
 input => output mapping for the model to learn?
+* In a symmetric TSP, there are two optimal paths for two directions of traversing the
+optimal circuit. This might make training harder because the network doesn't know which
+solution the training data has. Probably we want to somehow break the symmetry. We would
+think that this forces the model to coin-flip guess the first city which is 1/10 of choices
+so bakes in a minimum 5% error?
+    * add a little nonsymmetric noise to all distance matrices
+    * have a convention of favoring circuits that start at lower indices
+    * braeking the symmetry seems to reduce training loss a tiny bit, like .26 to .24
+
+# Best model so far
+```
+function _default_model(problem_size)
+    input_size = 2 * problem_size^2
+    output_size = problem_size
+    return Flux.Chain(
+        Flux.Dense(input_size, 200, Flux.gelu),
+        Flux.Dense(200, 80, Flux.gelu),
+        Flux.Dense(80, 35, Flux.gelu),
+        Flux.Dense(35, output_size, identity),
+        # logitcrossentropy loss, no softmax
+    )
+end
+```
+* This [with relu not gelu] is a wider variant of a previous model, which seems to help a tiny bit, maybe .28 => .25 train loss. Wider > deeper here?
+* Swapping relu for gelu seems to help a bunch with train loss, down to like .14, presumably some performance penalty?
+* Generating 40k rather than 30k training examples and adding another 80=>80 layer in the middle maybe gets train 
+loss down to ~.12, which is modest but helpful?
--- src/TravelingMachineLearner.jl
+++ src/TravelingMachineLearner.jl
@@ -1,8 +1,20 @@
 module TravelingMachineLearner
-using LinearAlgebra: norm
+
+import LinearAlgebra: norm
+import Random: Xoshiro, GLOBAL_RNG
+
+
+import StatsBase
+import Flux
+import Flux: gpu, cpu
+
 const TSP_SIZE = 10
 
 include("solve_tsp.jl")
+
+##
+# Helpers
+##
 
 """Map a path through one or more cities (must be non-empty and start at city 1) to a
 binary matrix of arc indicators. Roughly the inverse of `_extract_tour`."""
@@ -16,12 +28,39 @@ function _path_to_arc_mat(path, n_cities; type = Float32)
     return arc_indicators
 end
 
+function model_to_path(distmat, model; n_arcs = size(distmat, 1))
+    path = [1]
+    n_cities = size(distmat, 1)
+    for _ in 1:n_arcs
+        x = vcat(vec(distmat), vec(_path_to_arc_mat(path, n_cities)))
+        next_city_weights = Flux.softmax(model(x)) # model returns logits
+        next_city = StatsBase.sample(1:n_cities, StatsBase.Weights(next_city_weights))
+        push!(path, next_city)
+    end
+    return path
+end
+
+##
+# Data generation
+##
+
 """Map a TSP instance with `TSP_SIZE` cities to `TSP_SIZE` points of training data.
 Each observation is a single "next arc" step in one-hot encoded form. Return a tuple
 (X, Y) where X has `2 * TSP_SIZE^2` rows and `TSP_SIZE` columns, and `Y` is a square
-`TSP_SIZE` x `TSP_SIZE` matrix."""
-function tsp_to_data(dm, type = Float32)
+`TSP_SIZE` x `TSP_SIZE` matrix.
+
+If `break_symmetry`, prefer path directions that go from city 1 (always the start) to low
+index cities."""
+function tsp_to_data(dm, type = Float32; break_symmetry = true)
     path = solve_tsp(dm)
+    if break_symmetry
+        cost = sum(dm[path[i], path[i+1]] for i in 1:(length(path) - 1))
+        cost_rev = sum(dm[path[i+1], path[i]] for i in 1:(length(path) - 1))
+        can_reverse = abs(cost - cost_rev) < 1e5
+        if can_reverse && path[2] > path[end-1]
+            path = reverse(path)
+        end
+    end
     n_cities = size(dm, 1)
     @assert path[1] == path[end] == 1
     @assert length(path) == n_cities + 1
@@ -37,20 +76,90 @@ function tsp_to_data(dm, type = Float32)
     return X, Y
 end
 
-"""Generate the Euclidean distance matrix for `n` points in the unit square."""
-function generate_unit_square_distmat(n::Int = TSP_SIZE)
-    pts = rand(2, n)
-    return [norm(pts[:,i] - pts[:,j]) for i in 1:n, j in 1:n]
+"""Generate the Euclidean TSP for `n` points in the unit square. Return a named tuple
+with keys :pts and :distmat. If `order_points`, arrange points so that lower indices
+are more central."""
+function generate_unit_square_tsp(n::Int, rng = GLOBAL_RNG; order_points = false)
+    pts = rand(rng, 2, n)
+    distmat = [norm(pts[:,i] - pts[:,j]) for i in 1:n, j in 1:n]
+    if order_points
+        order = sum(distmat, dims=1) |> vec |> sortperm
+        distmat = distmat[order, order]
+        pts = pts[:, order]
+    end
+    return (;pts, distmat)
 end
 
 """Generate combined training data for `n_tsp` unit square Euclidean TSP instances, each
 with `TSP_SIZE` points. Return a tuple `(X, Y)`."""
-function generate_data(n_tsps, tsp_size = TSP_SIZE)
-    instances = [tsp_to_data(generate_unit_square_distmat(tsp_size)) for _ in 1:n_tsps]
+function generate_data(n_tsps, tsp_size = TSP_SIZE, rng = GLOBAL_RNG)
+    instances = [
+        tsp_to_data(generate_unit_square_tsp(tsp_size, rng).distmat)
+        for _ in 1:n_tsps
+    ]
     return (
         reduce(hcat, [inst[1] for inst in instances]), # X
         reduce(hcat, [inst[2] for inst in instances]), # Y
     )
+end
+
+function _default_model(problem_size)
+    input_size = 2 * problem_size^2
+    output_size = problem_size
+    return Flux.Chain(
+        Flux.Dense(input_size, 200, Flux.gelu),
+        Flux.Dense(200, 80, Flux.gelu),
+        Flux.Dense(80, 80, Flux.gelu),
+        Flux.Dense(80, 35, Flux.gelu),
+        Flux.Dense(35, output_size, identity),
+        # logitcrossentropy loss, no softmax
+    )
+end
+
+const INIT_MODEL = _default_model(TSP_SIZE)
+
+function train_model(;
+    tsp_size = TSP_SIZE,
+    model = _default_model(tsp_size),
+    loss_fn = Flux.Losses.logitcrossentropy,
+    n_tsps = 1_000,
+    rng = Xoshiro(47),
+    data = generate_data(n_tsps, tsp_size, rng),
+    n_epochs = 500,
+    batch_size = 1_000,
+    opt = Flux.Adam(.001),
+    use_gpu = false,
+    print_every = 25
+)
+    # set up data
+    X, Y = data
+    if use_gpu
+        X, Y = gpu(X), gpu(Y)
+    end
+    train_data = Flux.DataLoader((X, Y); batchsize = batch_size, shuffle = true, rng)
+    @info "Generated training data" size(X)
+
+    # prepare for training
+    loss(m, x, y) = loss_fn(m(x), y)
+    if use_gpu
+        model = gpu(model)
+    end
+    opt_state = Flux.setup(opt, model)
+    @info "Ready for training" use_gpu
+    train_losses = []
+    push!(train_losses, cpu(loss(model, X, Y)))
+
+    # main train loop
+    for epoch in 1:n_epochs
+        if epoch % print_every == 1
+            @info "Start of epoch $(epoch)" train_losses[end]
+        end
+        GC.gc(false)
+        Flux.train!(loss, model, train_data, opt_state)
+        push!(train_losses, cpu(loss(model, X, Y)))
+    end
+    @info "Done training" train_losses[end]
+    return cpu(model)
 end
 
 
